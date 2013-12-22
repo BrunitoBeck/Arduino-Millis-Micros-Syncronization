76acc7fd781c7b38efdcd028b62aa5e6d328153a
Aakash Patel <contact@aaka.sh>
2013-12-21 23:30:03 -0600

    Didn't do much

--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -1,9 +1,15 @@
 void setup() {
-  
-
+  syncTime(); // Sync time with TimeServer
 }
 
 void loop() {
  
-
 }
+
+void syncTime(){
+  Wire.begin();  // Initialize I2c
+  
+  Wire.requestFrom(0x50, 10);  // Request 10 
+  
+  
+} 
