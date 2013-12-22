eec7e924682d47cc20e4f9d8cb3da52b3332d08d
Aakash Patel <contact@aaka.sh>
2013-12-21 23:58:27 -0600

    Added untested read from time server

--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -1,4 +1,10 @@
+#include <Wire.h>
+
+char dataFromTimeServer[10]; 
+unsigned long timeFromTimeServer;
+
 void setup() {
+  Serial.begin(9600);
   syncTime(); // Sync time with TimeServer
 }
 
@@ -8,8 +14,14 @@ void loop() {
 
 void syncTime(){
   Wire.begin();  // Initialize I2c
-  
   Wire.requestFrom(0x50, 10);  // Request 10 
-  
-  
+  int index = 0;
+  while(Wire.available())    
+  { 
+    char c = Wire.read();    // receive a byte as character
+    dataFromTimeServer[index++] = c;
+    
+  }
+  timeFromTimeServer = atol(dataFromTimeServer);
+  Serial.println(timeFromTimeServer);
 } 
