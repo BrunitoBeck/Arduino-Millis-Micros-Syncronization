08dbc9db47a0e1a9e51acdd755266c37b4a5b234
Aakash Patel <contact@aaka.sh>
2013-12-22 22:54:24 -0600

    Cleaned up TimeServer code.

--- TimeServer/TimeServer.ino
+++ TimeServer/TimeServer.ino
@@ -1,26 +1,28 @@
 #include <Wire.h>
 
-String timeValue; // Variable to hold time
-char dataFromClient[10];
+String timeOffsetStr; // Variable to hold time
+char dataFromClient[10];  // Variable to hold data from client
 
-volatile unsigned long clientTime;
+volatile unsigned long clientTime;  // Variable to hold timestamp of client
+
+const int syncStatusPin = 11;  // Pin that sync server status is output on
+
 void setup()
 {
-  timeValue.reserve(10); // Reserve 10 bytes for unsigned long time
-  //clientTime.reserve(10); // Reserve 10 bytes for unsigned long time
+  timeOffsetStr.reserve(10); // Reserve 10 bytes for unsigned long time
   Wire.begin(0x50); // Setup I2C slave address as 0x50     
-  Wire.onRequest(requestEvent); // Register event
-  Wire.onReceive(receiveEvent); // Register event
-  pinMode(11, OUTPUT);  // setup pin 11 as output
-  digitalWrite(11, HIGH);  // set it to high
+  Wire.onRequest(requestEvent); // Register event for requested data
+  Wire.onReceive(receiveEvent); // Register event for incomming data
+  pinMode(syncStatusPin, OUTPUT);  // setup pin 11 as output
+  digitalWrite(syncStatusPin, HIGH);  // set it to high
 }
 
 void loop() {
  // Do nothing
 }
+
 void receiveEvent(int bytes){
-  digitalWrite(11, LOW); // let other arduinos know that we're busy
-  
+  digitalWrite(syncStatusPin, LOW); // let other arduinos know that we're busy
   int index = 0;  // index of char array
   while(Wire.available())   // while more bytes avail
   { 
@@ -30,42 +32,41 @@ void receiveEvent(int bytes){
   }
   clientTime = atol(dataFromClient);  // convert char array to unsigned long
 }
+
 void requestEvent()
 {
-
-  
-  timeValue = String(micros() - clientTime);  // convert timestamp to string
-  volatile int timeLength = timeValue.length();  // find out how many digits are in the current time
+  timeOffsetStr = String(micros() - clientTime);  // convert timestamp to string
+  volatile int timeLength = timeOffsetStr.length();  // find out how many digits are in the current time
   // If not 10 digits (max digit length for unsigned long), add leading zeros
   // This is to keep the time of I2C transfers consistent between subsequent read/writes
   if(timeLength == 1){
-    timeValue = "000000000" + timeValue;
+    timeOffsetStr = "000000000" + timeOffsetStr;
   }
   else if(timeLength == 2){
-    timeValue = "00000000" + timeValue;
+    timeOffsetStr = "00000000" + timeOffsetStr;
   }
   else if(timeLength == 3){
-    timeValue = "0000000" + timeValue;
+    timeOffsetStr = "0000000" + timeOffsetStr;
   }
   else if(timeLength == 4){
-    timeValue = "000000" + timeValue;
+    timeOffsetStr = "000000" + timeOffsetStr;
   }
   else if(timeLength == 5){
-    timeValue = "00000" + timeValue;
+    timeOffsetStr = "00000" + timeOffsetStr;
   }
   else if(timeLength == 6){
-    timeValue = "0000" + timeValue;
+    timeOffsetStr = "0000" + timeOffsetStr;
   }
   else if(timeLength == 7){
-    timeValue = "000" + timeValue;
+    timeOffsetStr = "000" + timeOffsetStr;
   }
   else if(timeLength == 8){
-    timeValue = "00" + timeValue;
+    timeOffsetStr = "00" + timeOffsetStr;
   }
   else if(timeLength == 9){
-    timeValue = "0" + timeValue;
+    timeOffsetStr = "0" + timeOffsetStr;
   }
-  Wire.print(timeValue); // respond with padded timestamp
+  Wire.print(timeOffsetStr); // respond with padded timestamp
   
-  digitalWrite(11, HIGH); // let other arduinos know that we're clear
+  digitalWrite(syncStatusPin, HIGH); // let other arduinos know that we're clear
 }
