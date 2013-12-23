3606abe5f1fadfd04fecd5efd0e5af6a9291291e
Aakash Patel <contact@aaka.sh>
2013-12-22 22:47:36 -0600

    Cleaned up Timer code.

--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -1,16 +1,15 @@
 #include <Wire.h>
 
 char dataFromTimeServer[10]; // char array to hold I2C data from TimeServer
-unsigned long timeFromTimeServer = 0;  // unsigned long to hold time from TimeServer
+unsigned long timeOffset = 0;  // unsigned long to hold time from TimeServer
 
 unsigned long currentSyncedTime = 0;
 unsigned long timeOfLastSync = 0;
 
 const int buttonPin = 10;
+const int syncStatusPin = 11;
 
-String timeValue; // Variable to hold time
 void setup() {
-  timeValue.reserve(10);
   
   Serial.begin(9600);
   
@@ -19,8 +18,8 @@ void setup() {
   digitalWrite(buttonPin,HIGH);  // enable pull up resitor
   attachInterrupt(buttonPin, buttonPress, FALLING);  // register interrup on FALLING state
   
-  digitalWrite(11,HIGH);  // enable pull up resitor
-  pinMode(11, INPUT);  // set it up as an input
+  digitalWrite(syncStatusPin,HIGH);  // enable pull up resitor
+  pinMode(syncStatusPin, INPUT);  // set it up as an input
 }
 
 
@@ -38,49 +37,52 @@ void loop() {
 // Run when falling
 void buttonPress(){
   // Record timestamp in reference frame of TimeServer
-  currentSyncedTime = timeFromTimeServer + micros();
+  currentSyncedTime = timeOffset + micros();
 }
 
 // Method to read timestamp from central TimeServer over I2C
 void syncTime(){
     //Serial.print("SYNCING...");
   Wire.begin();  // Initialize I2c  
-  Wire.beginTransmission(0x50);
-  timeValue = String(micros());  // convert timestamp to string
-  volatile int timeLength = timeValue.length();  // find out how many digits are in the current time
+  
+  Wire.beginTransmission(0x50);  // begin communication with TimeServer
+  String timeStr = String(micros());  // convert timestamp to string
+  
+  volatile int timeLength = timeStr.length();  // find out how many digits are in the current time
   // If not 10 digits (max digit length for unsigned long), add leading zeros
   // This is to keep the time of I2C transfers consistent between subsequent read/writes
   if(timeLength == 1){
-    timeValue = "000000000" + timeValue;
+    timeStr = "000000000" + timeStr;
   }
   else if(timeLength == 2){
-    timeValue = "00000000" + timeValue;
+    timeStr = "00000000" + timeStr;
   }
   else if(timeLength == 3){
-    timeValue = "0000000" + timeValue;
+    timeStr = "0000000" + timeStr;
   }
   else if(timeLength == 4){
-    timeValue = "000000" + timeValue;
+    timeStr = "000000" + timeStr;
   }
   else if(timeLength == 5){
-    timeValue = "00000" + timeValue;
+    timeStr = "00000" + timeStr;
   }
   else if(timeLength == 6){
-    timeValue = "0000" + timeValue;
+    timeStr = "0000" + timeStr;
   }
   else if(timeLength == 7){
-    timeValue = "000" + timeValue;
+    timeStr = "000" + timeStr;
   }
   else if(timeLength == 8){
-    timeValue = "00" + timeValue;
+    timeStr = "00" + timeStr;
   }
   else if(timeLength == 9){
-    timeValue = "0" + timeValue;
+    timeStr = "0" + timeStr;
   }
-  Wire.print(timeValue); // respond with padded timestamp
-  Wire.endTransmission();
+  
+  Wire.print(timeStr); // Send padded timestamp to TimeServer
+  Wire.endTransmission(); // End comm.
 
-  Wire.requestFrom(0x50, 10);  // Request 10 bytes
+  Wire.requestFrom(0x50, 10);  // Request 10 bytes (timestamp) from TimeServer
   int index = 0;  // index of char array
   while(Wire.available())   // while more bytes avail
   { 
@@ -88,7 +90,6 @@ void syncTime(){
     dataFromTimeServer[index++] = c;  // store byte in char array
     
   }
-  timeFromTimeServer = atol(dataFromTimeServer);  // convert char array to unsigned long
-  
+  timeOffset = atol(dataFromTimeServer);  // convert char array to unsigned long
   timeOfLastSync = millis();  // update time of last sync
 }  
