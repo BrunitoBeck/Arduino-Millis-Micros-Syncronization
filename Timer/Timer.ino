8a5cbfa5b79a34408d0d5079fd2a02be9347182d
Aakash Patel <contact@aaka.sh>
2013-12-22 21:10:46 -0600

    Added periodic sync code for Timer. Untested

--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -4,16 +4,19 @@ char dataFromTimeServer[10]; // char array to hold I2C data from TimeServer
 unsigned long timeFromTimeServer = 0;  // unsigned long to hold time from TimeServer
 
 unsigned long currentSyncedTime = 0;
+unsigned long timeOfLastSync = 0;
 
 const int buttonPin = 10;
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("SYNCING...");
   syncTime(); // Sync time with TimeServer
   
   digitalWrite(buttonPin,HIGH);  // enable pull up resitor
   attachInterrupt(buttonPin, buttonPress, FALLING);  // register interrup on FALLING state
+  
+  digitalWrite(11,HIGH);  // enable pull up resitor
+  pinMode(11, INPUT);  // set it up as an input
 }
 
 
@@ -22,6 +25,11 @@ void loop() {
    Serial.println(currentSyncedTime);
    currentSyncedTime=0;
  }
+ // Sync every 5 seconds if Time server is avail
+ if((millis() - timeOfLastSync) > 5000 && digitalRead(11) == LOW){ 
+   syncTime();
+ }
+
 }
 // Run when falling
 void buttonPress(){
@@ -31,6 +39,7 @@ void buttonPress(){
 
 // Method to read timestamp from central TimeServer over I2C
 void syncTime(){
+  Serial.print("SYNCING...");
   Wire.begin();  // Initialize I2c
   
   Wire.requestFrom(0x50, 10);  // Request 10 bytes
@@ -42,5 +51,5 @@ void syncTime(){
     
   }
   timeFromTimeServer = atol(dataFromTimeServer);  // convert char array to unsigned long
-  
+  timeOfLastSync = millis();  // update time of last sync
 } 
