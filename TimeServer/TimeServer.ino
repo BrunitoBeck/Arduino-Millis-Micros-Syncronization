c0ad43fdbcacb5e7072ca7472aebc1b3bb304343
Aakash Patel <contact@aaka.sh>
2013-12-22 22:36:11 -0600

    Apparently fully functional time sync code. New milestone.

--- TimeServer/TimeServer.ino
+++ TimeServer/TimeServer.ino
@@ -1,12 +1,16 @@
 #include <Wire.h>
 
 String timeValue; // Variable to hold time
+char dataFromClient[10];
 
+volatile unsigned long clientTime;
 void setup()
 {
   timeValue.reserve(10); // Reserve 10 bytes for unsigned long time
+  //clientTime.reserve(10); // Reserve 10 bytes for unsigned long time
   Wire.begin(0x50); // Setup I2C slave address as 0x50     
   Wire.onRequest(requestEvent); // Register event
+  Wire.onReceive(receiveEvent); // Register event
   pinMode(11, OUTPUT);  // setup pin 11 as output
   digitalWrite(11, HIGH);  // set it to high
 }
@@ -14,12 +18,23 @@ void setup()
 void loop() {
  // Do nothing
 }
-
-void requestEvent()
-{
+void receiveEvent(int bytes){
   digitalWrite(11, LOW); // let other arduinos know that we're busy
   
-  timeValue = String(micros());  // convert timestamp to string
+  int index = 0;  // index of char array
+  while(Wire.available())   // while more bytes avail
+  { 
+    char c = Wire.read();    // receive a byte as character
+    dataFromClient[index++] = c;  // store byte in char array
+    
+  }
+  clientTime = atol(dataFromClient);  // convert char array to unsigned long
+}
+void requestEvent()
+{
+
+  
+  timeValue = String(micros() - clientTime);  // convert timestamp to string
   volatile int timeLength = timeValue.length();  // find out how many digits are in the current time
   // If not 10 digits (max digit length for unsigned long), add leading zeros
   // This is to keep the time of I2C transfers consistent between subsequent read/writes
--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -8,8 +8,12 @@ unsigned long timeOfLastSync = 0;
 
 const int buttonPin = 10;
 
+String timeValue; // Variable to hold time
 void setup() {
+  timeValue.reserve(10);
+  
   Serial.begin(9600);
+  
   syncTime(); // Sync time with TimeServer
   
   digitalWrite(buttonPin,HIGH);  // enable pull up resitor
@@ -39,9 +43,43 @@ void buttonPress(){
 
 // Method to read timestamp from central TimeServer over I2C
 void syncTime(){
-  Serial.print("SYNCING...");
-  Wire.begin();  // Initialize I2c
-  
+    //Serial.print("SYNCING...");
+  Wire.begin();  // Initialize I2c  
+  Wire.beginTransmission(0x50);
+  timeValue = String(micros());  // convert timestamp to string
+  volatile int timeLength = timeValue.length();  // find out how many digits are in the current time
+  // If not 10 digits (max digit length for unsigned long), add leading zeros
+  // This is to keep the time of I2C transfers consistent between subsequent read/writes
+  if(timeLength == 1){
+    timeValue = "000000000" + timeValue;
+  }
+  else if(timeLength == 2){
+    timeValue = "00000000" + timeValue;
+  }
+  else if(timeLength == 3){
+    timeValue = "0000000" + timeValue;
+  }
+  else if(timeLength == 4){
+    timeValue = "000000" + timeValue;
+  }
+  else if(timeLength == 5){
+    timeValue = "00000" + timeValue;
+  }
+  else if(timeLength == 6){
+    timeValue = "0000" + timeValue;
+  }
+  else if(timeLength == 7){
+    timeValue = "000" + timeValue;
+  }
+  else if(timeLength == 8){
+    timeValue = "00" + timeValue;
+  }
+  else if(timeLength == 9){
+    timeValue = "0" + timeValue;
+  }
+  Wire.print(timeValue); // respond with padded timestamp
+  Wire.endTransmission();
+
   Wire.requestFrom(0x50, 10);  // Request 10 bytes
   int index = 0;  // index of char array
   while(Wire.available())   // while more bytes avail
@@ -51,5 +89,6 @@ void syncTime(){
     
   }
   timeFromTimeServer = atol(dataFromTimeServer);  // convert char array to unsigned long
+  
   timeOfLastSync = millis();  // update time of last sync
-} 
+}  
